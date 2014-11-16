2ea480d17c3c34b630de4f2a98755daabe55ab45
Hugo Roque <hugolnx@gmail.com>
2014-11-16 01:41:18 -0200

    Revert "creating of the poster with the description"
    This reverts commit a4c2b7255163f1754fbead368e558d55dd89df7f.

--- app/models/poster_image_uploader.rb
+++ app/models/poster_image_uploader.rb
@@ -8,23 +8,7 @@ class PosterImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [320, 480]
   end
 
-  version :with_text do
-    process :draw_text
-  end
-
   def store_dir
     "uploaded_posters/"
-  end
-private
-  def draw_text
-    manipulate! do |img|
-      img.combine_options do |c|
-        c.gravity 'Center'
-        c.pointsize '70'
-        c.draw "text 0,0 '#{model.description}'"
-        c.fill 'white'
-      end
-      img
-    end
   end
 end
