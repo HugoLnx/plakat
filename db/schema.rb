368f6a2f0e07e9acdf5af78f65120ea63a7aac02
Haz <hazdiego@gmail.com>
2015-02-01 02:51:31 -0200

    butterfree

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20141115044937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "categories", force: true do |t|
     t.string   "name"
