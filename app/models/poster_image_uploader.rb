397a6b100472386f0a5a4de7cdc24057cff28e0d
Hugo Roque <hugolnx@gmail.com>
2015-02-03 12:00:53 -0200

    bugfix: resizing image to widths 380(small) and 640(large)

--- app/models/poster_image_uploader.rb
+++ app/models/poster_image_uploader.rb
@@ -2,11 +2,11 @@ class PosterImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   version :large do
-    process resize_to_fit: [640, 0] # Resize to specific width and adjust height
+    process resize_to_limit: [640, 0] # Resize to specific width and adjust height
   end
 
   version :small do
-    process resize_to_fit: [380, 0]
+    process resize_to_limit: [380, 0]
   end
 
   def store_dir
