db1d226c08e06198038b2dbd3b28e9876871d551
Hugo Roque <hugolnx@gmail.com>
2015-02-03 12:09:26 -0200

    adding viewport to posters/show.html.erb to mobile viewining

--- app/models/poster_image_uploader.rb
+++ app/models/poster_image_uploader.rb
@@ -2,11 +2,11 @@ class PosterImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   version :large do
-    process resize_to_limit: [640, nil] # Resize to specific width and adjust height
+    process resize_to_fit: [640, nil] # Resize to specific width and adjust height
   end
 
   version :small do
-    process resize_to_limit: [380, nil]
+    process resize_to_fit: [380, nil]
   end
 
   def store_dir
