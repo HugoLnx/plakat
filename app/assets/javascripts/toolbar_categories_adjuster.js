9a4f7253bdcdc3d78449bc4c0e92084dc73718e4
Hugo Roque <hugolnx@gmail.com>
2015-02-03 21:48:09 -0200

    "static" show poster page: improving styling for mobile

--- app/assets/javascripts/toolbar_categories_adjuster.js
+++ app/assets/javascripts/toolbar_categories_adjuster.js
@@ -10,17 +10,21 @@ PLAKAT.ToolbarCategoriesAdjuster = (function() {
   var categoryItems = document.querySelectorAll(".categories-horizontal-list .category-item");
   var categoryExtra = document.querySelector(".categories-horizontal-list .extra-categories");
   var categoryExtraNumber = categoryExtra.querySelector(".extra-categories-number");
+  var categoriesLabel = categoryExtra.querySelector(".extra-categories-mobile-label");
 
   _M.adjustCategoriesToolbar = function() {
-    var isDesktop = window.innerWidth > 640;
+    var isDesktop = window.innerWidth >= 640;
     if (isDesktop && categoryItems.length > MAX_ITEMS_DESKTOP) {
       reduceCategoriesVisibleTo(MAX_ITEMS_DESKTOP)
-    } else if (!isDesktop && categoryItems.length > MAX_ITEMS_MOBILE) {
-      reduceCategoriesVisibleTo(MAX_ITEMS_MOBILE)
+    } else if (categoryItems.length > 0) {
+      categoryExtra.classList.remove(HIDDEN_CLASS);
+      categoryExtraNumber.classList.add(HIDDEN_CLASS);
+      categoriesLabel.classList.remove(HIDDEN_CLASS);
     }
   };
 
   function reduceCategoriesVisibleTo(qntCategories) {
+    categoriesLabel.classList.add(HIDDEN_CLASS);
     for(var i = 0; i < qntCategories; i++) {
       var categoryItem = categoryItems[i];
       categoryItem.classList.remove(HIDDEN_CLASS);
@@ -29,12 +33,12 @@ PLAKAT.ToolbarCategoriesAdjuster = (function() {
       var categoryItem = categoryItems[i];
       categoryItem.classList.add(HIDDEN_CLASS);
     }
-    console.log(qntCategories);
-    console.log(categoryItems.length);
     if (qntCategories === categoryItems.length) {
       categoryExtra.classList.add(HIDDEN_CLASS);
+      categoryExtraNumber.classList.add(HIDDEN_CLASS);
     } else {
       categoryExtra.classList.remove(HIDDEN_CLASS);
+      categoryExtraNumber.classList.remove(HIDDEN_CLASS);
     }
     categoryExtraNumber.textContent = categoryItems.length - qntCategories;
   }
