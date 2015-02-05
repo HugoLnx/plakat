71f1e1e5c80817d2dae823558e8febd835953426
Hugo Roque <hugolnx@gmail.com>
2015-02-05 17:49:34 -0200

    Back funcionando também na página estática

---
+++ app/assets/javascripts/lnx_utils/browser_utils.js
@@ -0,0 +1,10 @@
+if (typeof LNX_UTILS === "undefined") LNX_UTILS = {};
+if (typeof LNX_UTILS.BrowserUtils === "undefined") LNX_UTILS.BrowserUtils = {};
+
+(function(namespace) {
+  var BrowserUtils = namespace.BrowserUtils;
+
+  BrowserUtils.reloadPage = function() {
+    document.location.href = document.location.href;
+  };  
+}(LNX_UTILS));
--- app/assets/javascripts/poster_show.js
+++ app/assets/javascripts/poster_show.js
@@ -1,5 +1,6 @@
 //= require toolbar_categories_adjuster
 //= require toolbar_extra_categories
+//= require lnx_utils/browser_utils
 
 (function() {
   window.addEventListener("resize", function() {
@@ -9,4 +10,8 @@
   window.addEventListener("load", function() {
     PLAKAT.ToolbarCategoriesAdjuster.adjustCategoriesToolbar();
   });
+
+  window.onpopstate = function(event) {
+    LNX_UTILS.BrowserUtils.reloadPage();
+  };
 })();
