4d490233306396dc18cc347d4c79a5119c75c394
Hugo Roque <hugolnx@gmail.com>
2015-03-28 13:32:30 -0300

    Search by category, or content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
-  	@posters = Poster.all
+    @categories = Category.all
+    @term = params[:term]
+    @category = Category.find_by_name params[:category]
+    @posters = Poster.filter(term: @term, category: @category)
+      .page(params[:page])
+      .per(params[:per_page])
+      .order(created_at: :desc)
+      #.where("date_expiration >= ? AND posters.disabled = ? ", Time.now, false )
   end
 end
